110c9ecf27362da6c55a66078d6fd813723ebe2e
pepe <joshua.nario@gmail.com>
2018-05-27 01:22:00 -0400

    pretty admitfirst pipeline

--- admitFirst.go
+++ admitFirst.go
@@ -2,59 +2,63 @@ package r8lmt
 
 import "time"
 
+//admit-first pipelining, https://blog.golang.org/pipelines
+
 func NewAdmitFirstSpamChan (r8lmtChan chan interface{}, config *Config) chan interface{} {
 	//inspired by leo lara https://disqus.com/by/disqus_BI7TGHPb0v/
 	//first spamChan is admitted ("pass through") and begins the "reservation" (delay of time)
 	//returned channel is a channel expecting spammy inputs
 	//r8lmtChan is the ratelimited channel of the returned channel (spam of inputs)
-	spamChan := make(chan interface{}) //synchronous channel for pipelining
+	spamChan := make(chan interface{}) //pipelined channel for rx only, no need to close //todo add direction
 	var buffer interface{}             //data received from spammy channel
 	var ok bool
-	passthru := true	//flag to admit an spamChan after the end of reservation wait
+	passthru := true	//flag to admit a checkin after the end of reservation wait
 	var timer *time.Timer
-	sendbuf := func() {
+	admit := func(newdata interface{}) {
 		go func() {
-			r8lmtChan <-buffer
+			r8lmtChan<- newdata
 		}()
+	}
+	resetTimer := func() {
 		timer = time.NewTimer(config.Reservation)
 	}
-	waitpassthruinput := func() {
-		buffer, ok := <-spamChan
+	listenAndAdmitNextCheckin := func() {
+		buffer, ok := <-spamChan	//blocks for a "check in"
+		admit(buffer)	//admit a checkin
 		// If channel closed exit goroutine
 		if !ok {
-			go func() {
-				r8lmtChan <-buffer
-			}()
 			return
 		}
-		sendbuf()
+		resetTimer()
 	}
 	go func() {
 		defer close(r8lmtChan)
-		waitpassthruinput()	//wait for admitted spamChan then send to start next reservation wait
+		listenAndAdmitNextCheckin() //wait for admitted spamChan then send to start next reservation wait
 		for {
+			//todo needs thorough review
 			select {
 			case <-timer.C:	//reservation wait is over
-				if config.WillAdmitAfter { //send buffer at end of reservation
-					sendbuf()	//after send, check if spamChan is admitted or reserved
+				if config.WillAdmitAfter { //admit a checkin at end of reservation
+					admit(buffer)
+					resetTimer() //after admit and reset, check if state is open for admission or not
 					if passthru {
-						waitpassthruinput()	//next spamChan is not reserved and admitted to r8lmtChan
+						listenAndAdmitNextCheckin() //next checkin is not waitlisted and quickly admitted
 					} else {
 						passthru = true	//reset admission flag and restart the reservation wait
 						buffer = nil //reset buffer for next reservation
 					}
 				} else {	//discard buffer at end of reservation wait
-					waitpassthruinput() //just wait for next admitted spamChan
+					listenAndAdmitNextCheckin() //listen for checkin to be admitted
 				}
-			case buffer, ok = <-spamChan: //data was received from spamChan during the reservation wait
+			case buffer, ok = <-spamChan: //a checkin during the reservation wait
 				if !ok {
 					return
 				}
-				if config.IsReservationExpandable {	//extend the reservation wait by restarting the wait countdown
+				if config.IsReservationExpandable {	//debounce, extend the reservation wait by resetting the countdown
 					timer.Reset(config.Reservation)
 				}	//else do not extend the delay
-				if config.WillAdmitAfter { //send latest spamChan at end of delay a
-					passthru = false	//mark next spamChan as reserved
+				if config.WillAdmitAfter { //send latest checkin at end of delay a
+					passthru = false	//mark next checkin as reserved
 				}
 			}// chan sel
 		}// loop
